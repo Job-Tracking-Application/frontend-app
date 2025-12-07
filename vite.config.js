c7677cc463d732a8f003e8f9b7c12862b35db683
D6_92632_Vivek <bhosalevivek04@gmail.com>
2025-12-07 22:07:00 +0530

    refactor: Update vite.config.js to use command-based port configuration for preview

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  preview: {
-    allowedHosts: ["job-tracking-application-frontend.onrender.com"],
-    host: "0.0.0.0",
-    port: import.meta.env.PORT ? Number(import.meta.env.PORT) : 5173
-  }
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    preview: {
+      allowedHosts: ["job-tracking-application-frontend.onrender.com"],
+      host: "0.0.0.0",
+      // Render gives PORT only at runtime, so fallback for local
+      port: command === "preview" ? (process.env.PORT ? Number(process.env.PORT) : 5173) : 5173
+    }
+  };
 });
