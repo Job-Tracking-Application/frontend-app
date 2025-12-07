26c1679e38c323a8c85c5dd44d315146259cc651
D6_92632_Vivek <bhosalevivek04@gmail.com>
2025-12-07 22:01:27 +0530

    `Update vite.config.js with new preview settings and import statements`

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-env node */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    allowedHosts: ["job-tracking-application-frontend.onrender.com"],
+    host: "0.0.0.0",
+    port: import.meta.env.PORT ? Number(import.meta.env.PORT) : 5173
+  }
+});
